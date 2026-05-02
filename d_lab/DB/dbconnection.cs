df6b927282926fea6d9c39f5315c2396cae2affa
Esrael Worku <israeyob1995@gmail.com>
2026-05-02 23:03:11 +0300

    Add files via upload

---
+++ d_lab/DB/dbconnection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+
+namespace d.labdemo.DB
+{
+    static class DBConnection
+    {
+        static string connectionString = @"Data Source=HP-ELITE;Initial Catalog=d_lab;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+        public static SqlConnection checkConnection = new SqlConnection(connectionString);
+        private static string? query;
+
+
+        public static void intiate()
+        {
+            try
+            {
+                checkConnection.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"unable to connect to database: \n{ex.Message}", "db error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
